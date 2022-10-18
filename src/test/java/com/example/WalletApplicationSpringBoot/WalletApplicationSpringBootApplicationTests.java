35880a6070906adcb94e31ae64f70461f89f75dc
Anupal <patra.anupal@outlook.com>
2022-10-18 21:00:17 +0530

    Initial commit

---
+++ src/test/java/com/example/WalletApplicationSpringBoot/WalletApplicationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WalletApplicationSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
